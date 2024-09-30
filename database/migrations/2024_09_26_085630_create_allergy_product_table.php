259bfc474f6231d0105895df6c9051eeedd1b3c7
Lucas Toepoel <lucastoepoel@gmail.com>
2024-09-30 08:38:35 +0200

    changed migrations

--- database/migrations/2024_09_26_085630_create_allergy_product_table.php
+++ database/migrations/2024_09_26_085630_create_allergy_product_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('allergy_product');
+        Schema::dropIfExists('product_tag');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 Route::get('/', function () {
-    return redirect()->route('FoodBase.index');
+    return redirect()->route('Product.index');
 });
 Route::resource('Product', ProductController::class);
 Route::post('/decode-barcode', [ProductController::class, 'decode'])->name('decode.barcode');
-Route::post('/uploadbarcode', [ProductController::class, 'upload'])->name('FoodBase.uploadBarcode');
+Route::post('/uploadbarcode', [ProductController::class, 'upload'])->name('Product.uploadBarcode');
 
